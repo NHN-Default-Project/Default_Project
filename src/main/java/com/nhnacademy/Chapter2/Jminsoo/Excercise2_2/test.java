74d5fa534f580beba59a290b43ac022f95f5dce4
nannda3463 <minsu3463@gmail.com>
2023-09-17 22:12:09 +0900

    data copy

---
+++ src/main/java/com/nhnacademy/Chapter2/Jminsoo/Excercise2_2/test.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.Chapter2.Jminsoo.Excercise2_2;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test!");
+    }
+}
---
+++ src/main/java/com/nhnacademy/Chapter4/Jminsoo/Excercise4_2.java
@@ -0,0 +1,4 @@
+package com.nhnacademy.Chapter4.Jminsoo;
+
+public class Excercise4_2 {
+}
